2d1b0ed3a42d7309bc8645a03148ab933641d043
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-29 10:41:24 +0530

    Completed myserver example

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,12 @@
-const { myServer } = require('../index.js');
+const { myServer, Router } = require('../index.js');
+
+const router = new Router();
+const server = myServer(router);
+
+router.get('/', (req, res) => {
+  res.send('welcome to the world of misfit toys');
+});
+
+server.listen(3000, () => {
+  console.log('server is running on 3000');
+});
